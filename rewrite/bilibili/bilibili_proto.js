90f9b23152d32617116114cfdad1a06beb3e11af
JiFengQAQ <49270917+JiFengQAQ@users.noreply.github.com>
2024-04-22 11:54:32 +0800

    Update bilibili_proto.js

--- rewrite/bilibili/bilibili_proto.js
+++ rewrite/bilibili/bilibili_proto.js
@@ -185,6 +185,7 @@ const DataBase = {
 														case true:
 														default:
 															let data = ViewReply.fromBinary(body);
+															if(isDebug)$.log(`?? ${$.name}`, data);
 															if (data.cms?.length) {
 																data.cms = [];
 																if(isDebug)$.log(`?? ${$.name}`, "播放页广告卡片去除");
